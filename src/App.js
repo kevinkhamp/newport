b686fc4dfc1b74c56130216f3ebfaa37c57b4b11
Kevin <khampkevin@gmail.com>
2023-10-13 19:37:46 -0400

    sidebar boiler implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Sidebar from "./components/sidebar";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Sidebar/>
+
   )
 }
